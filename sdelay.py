9eb990146bfd435d84f3debe16ae13e8ccfc83f9
Liam Fraser <liam@liamfraser.co.uk>
2014-02-26 12:00:47 +0000

    Should all be working now

--- sdelay.py
+++ sdelay.py
@@ -29,6 +29,57 @@ class SoftDelay:
         # Multiply that by how long we need to delay for in seconds
         return i_sec * self.delay_time
 
+    @property
+    def register_count(self):
+        """
+        Work out the number of registers needed. The picoblaze registers are 8
+        bits, which means we each one can store the value 0-255 as a binary
+        integer.
+
+        Each increment instruction takes 2 instructions, an ADD and a JUMP. The
+        outer loop is negligible because, at most, it will only add 512
+        instructions to the total, so we won't really take that into acccount.
+        Hence, the division of the number of dummy instructions by
+        2^(registers-1) in the loop below.
+        """
+
+        registers = 1
+        while ((self.dummy_i / (2**(registers-1))) > (256**registers)):
+            registers += 1
+
+        return registers
+
+    @property
+    def outer_loops(self):
+        """
+        Work out the number of times the outer loop needs to loop to get the
+        rough number of increments we want. Sadly we have no control over the
+        inner loops so can't be too specific.
+        """
+
+        count = (self.dummy_i / (2**(self.register_count-1))) / 256
+        
+        # Round the number up
+        count = int(math.ceil(count))
+
+        return count
+
+    @property
+    def register_array(self):
+        """
+        Create an array for the number of registers we need, with the outer
+        loop register as the last one in the array.
+        """
+        
+        registers = []
+        for i in range(0, self.register_count):
+            registers.append(0)
+
+        # Set the appropriate number of outer loops
+        registers[-1] = 256 - self.outer_loops
+
+        return registers
+
     def generate(self):
         """
         We're going to generate assembly that uses nested for loops to create
@@ -48,41 +99,30 @@ class SoftDelay:
         This is a nested for loop that loops 256^2 times
         """
 
-        # Work out the number of registers we need. The number of dummy
-        # instructions is divided by the number of registers * 2, because each
-        # increment instruction is an increment followed by a jump. We use 256
-        # instead of 255 because the loop stops when there is an overflow from
-        # 255 to 0. 
-        registers = 1
-        while ((self.dummy_i / (registers * 2)) > (256**registers)):
-            registers += 1
-
-        # Work out the rough number of times we want to count to 256 and round
-        # up
-        count_num = int(math.ceil((self.dummy_i / (registers)) / 256))
-
-        print count_num
-
-        # Create a loop to do just that. We want 256 - the number of outer
-        # loops we need to do so that we overflow
-        s1 = 256 - count_num
-
         # Generate appropriate assembly
         out = "; Initialise loop registers\n"
-        out += "LOAD S0, {0}\n".format(hex(0))
-        out += "LOAD S1, {0}\n\n".format(hex(s1))
+
+        # Initialise the registers
+        for i in range(0, len(self.register_array)):
+            out += "LOAD S{0:x}, {1:#x}\n".format(i, self.register_array[i])
+
+        out += "\n"
         out += "; Loop for approx {0} seconds\n".format(self.delay_time)
         out += "loop:\n"
-        out += "    ADD S0, 0x01\n"
-        out += "    JUMP NZ, loop\n"
-        out += "    ADD S1, 0x01\n"
-        out += "    JUMP NZ, loop\n"
+
+        # Do the increments
+
+        for i in range(0, len(self.register_array)):
+            out += "ADD S{0:x}, 0x01\n".format(i, self.register_array[i])
+            out += "    JUMP NZ, loop\n"
 
         return out
 
 if __name__ == "__main__":
     # 2ms
     sd = SoftDelay(10, 2, 2 * (10**-3))
+    print sd.register_count
+    print sd.outer_loops
     # 1s
     #sd = SoftDelay(10, 2, 1)
     print sd.generate()
